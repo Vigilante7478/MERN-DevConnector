26f10480559211510d47a6aea2fef309148727e8
Vigilante7478 <vanditthakkar09@gmail.com>
2023-04-10 23:51:01 -0400

    Added Profile, Education and Experiences

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -37,6 +41,22 @@ const App = () => {
           <Route
             path='/dashboard'
             element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path='/create-profile'
+            element={<PrivateRoute component={CreateProfile} />}
+          />
+          <Route
+            path='/edit-profile'
+            element={<PrivateRoute component={EditProfile} />}
+          />
+          <Route
+            path='/add-experience'
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path='/add-education'
+            element={<PrivateRoute component={AddEducation} />}
           />
         </Routes>
       </Router>
