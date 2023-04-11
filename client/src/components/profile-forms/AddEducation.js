a2af41e21cc02eae27636aee26dabb6fe23d4b40
Vigilante7478 <vanditthakkar09@gmail.com>
2023-04-11 00:19:30 -0400

    Listed and deleted Experiences, Educations and Account and

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -89,7 +89,7 @@ const AddEducation = ({ addEducation }) => {
                 toggleDisabled(!toDateDisabled);
               }}
             />{' '}
-            Current Job
+            Current School
           </p>
         </div>
         <div className='form-group'>
