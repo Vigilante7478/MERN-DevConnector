548d87a709fddd27b293ab244353f6899a04c5a7
Vigilante7478 <vanditthakkar09@gmail.com>
2023-04-12 23:34:19 -0400

    Added Profile display section

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const axios = require('axios');
 const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -309,24 +310,38 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @access   Public
 router.get('/github/:username', async (req, res) => {
   try {
-    const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
-      method: 'GET',
-      headers: { 'user-agent': 'node.js' },
+    // const options = {
+    //   uri: `https://api.github.com/users/${
+    //     req.params.username
+    //   }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+    //     'githubClientId'
+    //   )}&client_secret=${config.get('githubSecret')}`,
+    //   method: 'GET',
+    //   headers: {
+    //     'user-agent': 'node.js',
+    //   },
+    // };
+    // request(options, (error, response, body) => {
+    //   if (error) console.error(error);
+
+    //   if (response.statusCode !== 200) {
+    //     return res.status(404).json({ msg: 'No Github profile found' });
+    //   }
+    //   res.json(JSON.parse(body));
+    // });
+
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`,
     };
 
-    request(options, (error, response, body) => {
-      if (error) console.error(error);
+    const gitHubResponse = await axios.get(uri, headers);
 
-      if (response.statusCode !== 200) {
-        return res.status(404).json({ msg: 'No Github profile found' });
-      }
-      res.json(JSON.parse(body));
-    });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
