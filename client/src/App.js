95df8c637dbaa9cf62e1fe2d387cd42990d034ba
Vigilante7478 <vanditthakkar09@gmail.com>
2023-04-14 09:01:22 -0400

    Added Posts & Comments

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -61,6 +63,11 @@ const App = () => {
           <Route
             path='/add-education'
             element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path='/posts' element={<PrivateRoute component={Posts} />} />
+          <Route
+            path='/posts/:id'
+            element={<PrivateRoute component={Post} />}
           />
         </Routes>
       </Router>
